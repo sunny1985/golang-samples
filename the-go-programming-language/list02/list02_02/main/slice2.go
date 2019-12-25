47e6c17f1f278e68b26dbbb1a47360db29e1cbf0
huangyinglu <huang_ying_lu@163.com>
2019-12-26 01:04:05 +0800

    map的定义和使用

--- the-go-programming-language/list02/list02_02/main/slice2.go
+++ the-go-programming-language/list02/list02_02/main/slice2.go
@@ -1,5 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	mySlice := make()
+	mySlice := make([]int, 5, 10)
+	fmt.Println("len(mySlice):", len(mySlice)) // 元素个数
+	fmt.Println("cap(mySlice):", cap(mySlice)) // 分配空间大小
 }
